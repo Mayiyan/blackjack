73383774bc801623b8bde169515b921d38b8dd6b
Juliana Duperron <juliana.duperron@omadahealth.com>
2019-08-06 09:57:38 -0700

    Fixed some things. Now the dealer's card is hidden until the real end of round.

--- src/App.js
+++ src/App.js
@@ -20,37 +20,56 @@ const initialState = () => {
     deck: deck,
     dealerHand: deck.getCards(2),
     hideFirstCard: true,
-    player1: {
-      handCards: deck.getCards(2),
-      stand: false,
-      busted: false,
-      winner: 'YOU'
-    },
-    player2: {
-      handCards: deck.getCards(2),
-      stand: false,
-      busted: false,
-      winner: 'NOT YOU',
-    }
+    players:
+      {
+        player1: {
+          handCards: deck.getCards(2),
+          stand: false,
+          busted: false,
+          winner: ''
+        },
+        player2: {
+          handCards: deck.getCards(2),
+          stand: false,
+          busted: false,
+          winner: '',
+        }
+      }
   }
+};
+
+const getPlayer = (playerName, state) => {
+  return state.players[playerName];
 };
 
 class App extends React.Component {
   state = initialState();
 
-  getCard = (player) => {
-    const handCardsPlayer = [...this.state[player].handCards, ...this.state.deck.getCards(1)];
+  getCard = (playerName) => {
+    const player = getPlayer(playerName, this.state);
+    const handCardsPlayer = [...player.handCards, ...this.state.deck.getCards(1)];
 
     const bustedPlayer = scoreCalculator(handCardsPlayer) > 21;
 
-    const playerState = { ...this.state[player], handCards: handCardsPlayer, busted: bustedPlayer, winner: bustedPlayer ? 'dealer' : 'You' };
+    const playerState = {
+      players:
+        {
+          ...this.state.players,
+          [playerName]: {
+            ...player,
+            handCards: handCardsPlayer,
+            busted: bustedPlayer,
+            winner: bustedPlayer ? 'dealer' : 'You'
+          }
+        }
+    };
 
-    this.setState({[player]: playerState});
+    this.setState(playerState);
   };
 
   endRound = (player) => {
-    const {deck, dealerHand, [player]: {winner, handCards}} = this.state;
-
+    const {deck, dealerHand} = this.state;
+    const {winner, handCards} = getPlayer(player, this.state);
     let newDealerHand = dealerHand;
     let newWinner = winner;
 
@@ -63,20 +82,34 @@ class App extends React.Component {
       newWinner = 'dealer';
     }
 
-    const playerState = { ...this.state[player], winner: newWinner, stand: true };
+    const playerState = {...this.state.players[player], winner: newWinner, stand: true};
 
-    this.setState({deck: deck, dealerHand: newDealerHand, [player]: playerState, hideFirstCard: false})
+    this.setState({
+      deck: deck,
+      dealerHand: newDealerHand,
+      players: {...this.state.players, [player]: playerState}
+    }, () => (this.setState({ hideFirstCard: !this.gameOverEveryone(this.state) })))
   };
 
   restartGame = () => {
     this.setState(initialState());
   };
 
+  gameOver = (player, state) => {
+    const {stand, busted} = getPlayer(player, state);
+    return stand || busted;
+  };
+
+  gameOverEveryone = (state) => {
+    const gameOverMan1 = this.gameOver('player1', state);
+    const gameOverMan2 = this.gameOver('player2', state);
+    return gameOverMan1 && gameOverMan2;
+  };
+
   render() {
-    const {dealerHand, hideFirstCard, player1, player2} = this.state;
-    const gameOverMan1 = player1.stand || player1.busted;
-    const gameOverMan2 = player2.stand || player2.busted;
-    const gameOverEveryone = gameOverMan1 && gameOverMan2;
+    const {dealerHand, hideFirstCard} = this.state;
+    const player1 = getPlayer('player1', this.state);
+    const player2 = getPlayer('player2', this.state);
     return (
       <StyledApp>
         <h1>Blackjack!</h1>
@@ -85,11 +118,13 @@ class App extends React.Component {
           <Hand cards={dealerHand} hideFirstCard={hideFirstCard}/>
           {(player1.stand || player2.stand) && <div>The dealer's score is {scoreCalculator(dealerHand)}</div>}
         </div>
-        {gameOverEveryone && <CompletionTile restartGame={this.restartGame} winner={player1.winner}/>}
+        {this.gameOverEveryone(this.state) && <CompletionTile restartGame={this.restartGame} winner={player1.winner}/>}
         <div>
-          <Player cards={player1.handCards} busted={player1.busted} gameOverMan={gameOverMan1} getCard={() => this.getCard('player1')}
+          <Player cards={player1.handCards} busted={player1.busted} gameOverMan={this.gameOver('player1', this.state)}
+                  getCard={() => this.getCard('player1')}
                   endRound={() => this.endRound('player1')} name="Me!"/>
-          <Player cards={player2.handCards} busted={player2.busted} gameOverMan={gameOverMan2} getCard={() => this.getCard('player2')}
+          <Player cards={player2.handCards} busted={player2.busted} gameOverMan={this.gameOver('player2', this.state)}
+                  getCard={() => this.getCard('player2')}
                   endRound={() => this.endRound('player2')} name="Not Me!"/>
         </div>
       </StyledApp>
@@ -97,6 +132,7 @@ class App extends React.Component {
   }
 }
 
+// TOMORROW: REDUUUUUUUUUUX
 // The dealer score should only be calculated after the game is over
 // Show individual winners (you lose or not basically)
 // currently, as long as the dealer's score is less than 17 they keep hitting. There's no need to do this.
